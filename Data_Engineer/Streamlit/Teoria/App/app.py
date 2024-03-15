4ae1d9aab7cdfc63835e96d39d47cdec9966a168
XimenaPO <145386168+XimenaPO@users.noreply.github.com>
2024-03-15 18:45:02 +0100

    upload correcciones lecciones de Flask

---
+++ Data_Engineer/Streamlit/Teoria/App/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('Hola mundo, esto es un título ')
+
+st.subheader('Esto es un subtitulo')
+
+st.text('Esto es un texto')
+
+st.text('Esto se puede usar para otras cosas')
